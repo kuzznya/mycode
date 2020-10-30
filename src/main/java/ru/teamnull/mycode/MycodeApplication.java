39e877ed76058f5a3b28c394c5a500eb9500db63
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-10-30 19:00:04 +0300

    Initial commit

---
+++ src/main/java/ru/teamnull/mycode/MycodeApplication.java
@@ -0,0 +1,13 @@
+package ru.teamnull.mycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/teamnull/mycode/MycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.teamnull.mycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
