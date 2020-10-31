5f2b542225639d22f44a9d8305431920689ec2db
MaksimGolish <maksim.golish2002@gmail.com>
2020-10-31 16:10:15 +0300

    fix: Remove Runner logic

--- src/main/java/ru/teamnull/mycode/TestRunner.java
+++ src/main/java/ru/teamnull/mycode/TestRunner.java
@@ -28,58 +28,59 @@ public class TestRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Group group = new Group(null, "M3205", Collections.emptyList(), Collections.emptySet());
-        groupRepository.save(group);
-        User user = new User(
-                null,
-                "kuzznya",
-//                passwordEncoder.encode("test"),
-                "test",
-                "Ilya",
-                "Kuznetsov",
-                "Andreevich",
-                "ikuz2002@gmail.com",
-                new Date(),
-                Role.STUDENT,
-                group,
-                Collections.emptyList()
-        );
-        userRepository.save(user);
-        Task task = new Task(
-                null,
-                "avl",
-                List.of(group),
-                "test",
-                List.of(new Sample(null, "1", "5")),
-                new Date(), 5.0f, 5,
-                CheckType.TEST,
-                PostprocessorType.EASY,
-                Collections.emptyList(),
-                null
-        );
-        taskRepository.save(task);
-        Test test = new Test(5, "10 0", "something", 1.0f);
-        task.setTests(List.of(test));
-        testRepository.save(test);
-        group.setTasks(List.of(task));
-
-        taskRepository.save(task);
-        groupRepository.save(group);
-        Submission submission = Submission.builder()
-                .user(user)
-                .status(SubmissionStatus.OK)
-                .timestamp(new Date())
-                .build();
-        Submission submission1 = Submission.builder()
-                .user(user)
-                .status(SubmissionStatus.OK)
-                .timestamp(new Date())
-                .build();
-        submissionRepository.save(submission);
-        submissionRepository.save(submission1);
-//        submissionRepository.deleteById(submission1.getId());
-//        taskRepository.deleteById(task.getId());
-        userRepository.getOne(user.getId());
-//        userRepository.deleteById(user.getId());
+        return;
+//        Group group = new Group(null, "M3205", Collections.emptyList(), Collections.emptySet());
+//        groupRepository.save(group);
+//        User user = new User(
+//                null,
+//                "kuzznya",
+////                passwordEncoder.encode("test"),
+//                "test",
+//                "Ilya",
+//                "Kuznetsov",
+//                "Andreevich",
+//                "ikuz2002@gmail.com",
+//                new Date(),
+//                Role.STUDENT,
+//                group,
+//                Collections.emptyList()
+//        );
+//        userRepository.save(user);
+//        Task task = new Task(
+//                null,
+//                "avl",
+//                List.of(group),
+//                "test",
+//                List.of(new Sample(null, "1", "5")),
+//                new Date(), 5.0f, 5,
+//                CheckType.TEST,
+//                PostprocessorType.EASY,
+//                Collections.emptyList(),
+//                null
+//        );
+//        taskRepository.save(task);
+//        Test test = new Test(5, "10 0", "something", 1.0f);
+//        task.setTests(List.of(test));
+//        testRepository.save(test);
+//        group.setTasks(List.of(task));
+//
+//        taskRepository.save(task);
+//        groupRepository.save(group);
+//        Submission submission = Submission.builder()
+//                .user(user)
+//                .status(SubmissionStatus.OK)
+//                .timestamp(new Date())
+//                .build();
+//        Submission submission1 = Submission.builder()
+//                .user(user)
+//                .status(SubmissionStatus.OK)
+//                .timestamp(new Date())
+//                .build();
+//        submissionRepository.save(submission);
+//        submissionRepository.save(submission1);
+////        submissionRepository.deleteById(submission1.getId());
+////        taskRepository.deleteById(task.getId());
+//        userRepository.getOne(user.getId());
+////        userRepository.deleteById(user.getId());
     }
 }
