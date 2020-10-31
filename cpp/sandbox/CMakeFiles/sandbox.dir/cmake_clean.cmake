1ac911ff798314e6d926d5116e7b25fd9dd7f988
Nikolay Fedotenko <vafedotenko@gmail.com>
2020-10-31 11:24:25 +0300

    Add sandbox

---
+++ cpp/sandbox/CMakeFiles/sandbox.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sandbox.dir/error.c.o"
+  "CMakeFiles/sandbox.dir/platform/linux/memusage.c.o"
+  "CMakeFiles/sandbox.dir/platform/linux/setlimits.c.o"
+  "CMakeFiles/sandbox.dir/safe.c.o"
+  "CMakeFiles/sandbox.dir/sandbox.c.o"
+  "CMakeFiles/sandbox.dir/setlimit.c.o"
+  "sandbox"
+  "sandbox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
