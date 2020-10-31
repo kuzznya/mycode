55fe1bfc3be1b137a7605e4522a10a80a3daca3e
Nikolay Fedotenko <vafedotenko@gmail.com>
2020-10-31 11:44:39 +0300

    Remove dump files

--- cpp/sandbox/CMakeFiles/permission.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/permission"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/permission.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
