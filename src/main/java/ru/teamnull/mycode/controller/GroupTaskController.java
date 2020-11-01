9e06496a6310c22deae65ea8c06388cf201f42d3
MaksimGolish <maksim.golish2002@gmail.com>
2020-11-01 08:26:41 +0300

    refactor: minor changes

--- src/main/java/ru/teamnull/mycode/controller/GroupTaskController.java
+++ src/main/java/ru/teamnull/mycode/controller/GroupTaskController.java
@@ -1,27 +1,16 @@
 package ru.teamnull.mycode.controller;
 
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import ru.teamnull.mycode.dto.GroupsTask;
 import ru.teamnull.mycode.service.GroupTaskService;
 
-import java.util.List;
-
 @RestController("/tasks")
 @AllArgsConstructor
 public class GroupTaskController {
     private final GroupTaskService groupTaskService;
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
     @GetMapping("/tasks")
     public Flux<GroupsTask> getGroupTasks() {
